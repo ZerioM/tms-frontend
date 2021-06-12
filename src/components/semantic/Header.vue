3a77baf5bdc0b52c729566fbf24d840c5c5882d1
it201527 <it201527@fhstp.ac.at>
2021-06-12 20:16:59 +0200

    Adds Menu functionality

--- src/components/semantic/Header.vue
+++ src/components/semantic/Header.vue
@@ -1,20 +1,41 @@
 <template>
   <div id="header">
       <img id="back-m" src="@/assets/icons/big_m.svg" />
-      <NavBar />
-      <Title :text="title" id="title" />
-      <SearchInput />
+      <div id="nav-bar">
+        <UserIcon v-bind:class="{ hidden: isMenuVisible }" imageSrc="/images/standard/user.svg" username="User_23456" id="user-icon" />
+        <router-link v-bind:class="{ hidden: isMenuVisible }" id="logo" to="/">
+            <img src="@/assets/icons/tms_white.svg" />
+        </router-link>
+        <Menu id="menu" v-bind:class="{ displaynone: !isMenuVisible }" />
+        <MenuIcon id="menu-icon" @click="toggleMenu()"/>
+      </div>
+      <Title v-bind:class="{ hidden: isMenuVisible }" :text="title" id="title" />
+      <SearchInput v-if="searchable && !isMenuVisible" />
   </div>
 </template>
 
 <script>
-import NavBar from '../elements/NavBar.vue'
-import SearchInput from '../elements/SearchInput.vue'
-import Title from '../elements/Title.vue'
+import SearchInput from '../elements/SearchInput.vue';
+import Title from '../elements/Title.vue';
+import Menu from './Menu.vue';
+import UserIcon from '../elements/UserIcon.vue';
+import MenuIcon from '../elements/MenuIcon.vue';
+
 export default {
-  components: { SearchInput, Title, NavBar },
+  components: { SearchInput, Title, Menu, UserIcon, MenuIcon },
   props: {
-      title: String
+      title: String,
+      searchable: Boolean
+  },
+  data () {
+    return {
+        isMenuVisible: false,
+    }
+  },
+  methods: {
+    toggleMenu() {
+        this.isMenuVisible = !this.isMenuVisible;
+    }
   }
 }
 </script>
@@ -35,9 +56,52 @@ export default {
     max-width: 102%;
     width: 102%;
     height: auto;
+    pointer-events: none;
 }
 
 #title {
     align-content: center;
+}
+
+.displaynone {
+    display: none;
+}
+
+.hidden {
+    visibility: hidden;
+}
+
+#nav-bar {
+    display: flex;
+    justify-content: space-between;
+}
+
+#logo {
+    margin: 0;
+    margin-top: 2.5%;
+    margin-bottom: 5%;
+    margin-left: -5%;
+    z-index: 1;
+}
+
+#user-icon {
+    margin-left: 3.86%;
+    padding-top: 2%;
+    z-index: 2 ;
+}
+
+#menu {
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: auto;
+}
+
+#menu-icon {
+    margin-top: 5%;
+    margin-right: 3.86%;
+    margin-left: 3%;
+    z-index: 1000;
 }
 </style>
---
+++ src/components/semantic/Menu.vue
@@ -0,0 +1,88 @@
+<template>
+<div id="menu">
+    <img id="back-m" src="@/assets/icons/big_m.svg" />
+    <nav>
+        <h2>Menü</h2>
+        <hr/>
+        <ul id="navigation">
+            <li><router-link class="nav-list-el" to="/">Home</router-link></li>
+            <li><router-link class="nav-list-el" to="/fridges">Monitoring</router-link></li>
+            <li><router-link class="nav-list-el" to="/information">Information</router-link></li>
+            <li><router-link class="nav-list-el" to="/">Impressum</router-link></li>
+        </ul>
+    </nav>
+</div> 
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+#menu {
+    width: 100%;
+    height: 250px;
+    padding: 0%;
+    padding-top: 5%;
+    text-align: center;
+    background: dodgerblue;
+}
+ 
+#back-m {
+    z-index: 0;
+    top: -30px;
+    left: -10px;
+    position: absolute;
+    opacity: 0.3;
+    max-width: 102%;
+    width: 102%;
+    height: auto;
+    pointer-events: none;
+}
+
+h2 {
+    margin-top: 0%;
+    font-family: Spartan;
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 22px;
+    letter-spacing: 0px;
+    color: white;
+}
+
+ul {
+    margin-left: -10%;
+}
+
+li {
+    margin-bottom: 7.5%;
+}
+
+hr {
+    border-top: 2px solid white;
+    width: 104px;
+}
+
+#horizontal-line {
+    border: 5px white;
+}
+
+#navigation {
+    list-style-type: none;
+}
+
+.nav-list-el {
+    font-family: Spartan;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 18px;
+    letter-spacing: 0px;
+    margin-bottom: 20%;
+    color: white;
+    text-decoration: none;
+}
+</style>
